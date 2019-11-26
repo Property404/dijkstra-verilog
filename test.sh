f50f7af9d146554b7a64c4b9512057a90a8bbaf1
Dagan N Martinez <dnm0012@uah.edu>
2019-11-25 19:22:36 -0600

    test.sh don't explicitly show test bench extensions

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -e
 SRC_DIR=src
 OUT_FILE=testbench_results.txt
 TESTBENCH_DIR=${SRC_DIR}/testbenches/
-TESTBENCHES=(block_ram_tb.v priority_queue_tb.sv top_tb.sv writer_tb.sv edge_cache_tb.sv)
+TESTBENCHES=(block_ram_tb priority_queue_tb writer_tb edge_cache_tb top_tb)
 
 echo "TESTBENCH RESULTS" > $OUT_FILE
 echo "Generated $(date)" >> $OUT_FILE
@@ -13,7 +13,7 @@ for testbench in "${TESTBENCHES[@]}"
 do
 	echo "Testbench: ${testbench}" >> $OUT_FILE
 	./clean.sh
-	vcs -full64 -sverilog -debug_all src/*.v src/*.sv src/testbenches/block_ram.v ${TESTBENCH_DIR}${testbench} +incdir+src
+	vcs -full64 -sverilog -debug_all src/*.v src/*.sv src/testbenches/block_ram.v ${TESTBENCH_DIR}${testbench}* +incdir+src
 	./simv | tee -a $OUT_FILE
 	echo "~~~~~~~~~~~~~~~~~~" >> $OUT_FILE
 	echo >> $OUT_FILE
