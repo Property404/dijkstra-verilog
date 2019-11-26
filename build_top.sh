281054ed14e66af8dd69525733ed0ea745816bc6
Dagan N Martinez <dnm0012@uah.edu>
2019-11-25 19:23:15 -0600

    Correct buildtop to only build top and not testbench

--- build_top.sh
+++ build_top.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-#Build top testbench
-
-SRC_DIR=src
-OUT_FILE=testbench_results.txt
-TESTBENCH_DIR=${SRC_DIR}/testbenches/
-TESTBENCH=${TESTBENCH_DIR}/top_tb.sv
+#Build top level design
 
 ./clean.sh
-vcs -full64 -sverilog -debug_all src/*.v src/*.sv ${TESTBENCH_DIR}block_ram.v ${TESTBENCH} +incdir+src
+vcs -full64 -sverilog -debug_all src/*.v src/*.sv +incdir+src
